c9865418c8a11b459ceff2a8d44401d09d7b1c75
Ice Maya <echaymaya@gmail.com>
2019-03-17 15:23:08 +1300

    modified layout in app component using semantic container. Also passed CustomerFilter child component

--- src/component/App.js
+++ src/component/App.js
@@ -1,14 +1,16 @@
 import React, {Component} from 'react'
+import CustomerFilter from './CustomerFilter'
+import {Container} from 'semantic-ui-react'
 
 class App extends Component {
 
     render() { 
         return ( 
-            <div>
-                <h1>Trial</h1>
-            </div>
-         );
+            <Container>
+                <CustomerFilter />
+            </Container>
+        )
     }
 }
  
-export default App;
+export default App
